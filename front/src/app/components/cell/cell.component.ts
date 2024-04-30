1ae9b7c90a0fecd670a3dafec2f60cc2ef6804cf
Lupitot <paul.piauger2@gmail.com>
2024-04-30 11:54:57 +0200

    change of the grid generation system

--- front/src/app/components/cell/cell.component.ts
+++ front/src/app/components/cell/cell.component.ts
@@ -1,38 +1,18 @@
-import { Component, Input, SimpleChange} from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'cell',
   standalone: true,
   imports: [],
   templateUrl: './cell.component.html',
-  styleUrl: './cell.component.css'
+  styleUrl: './cell.component.css',
 })
-
 export class CellComponent {
-
   @Input() value!: number;
 
-  currentValue!: number;
+  currentValue: number = 0;
 
   changeValue() {
-    this.currentValue = this.value;
-    console.log('changeValue', this.currentValue);
+    this.currentValue = (this.currentValue + 1) % 6;
   }
-
-  changeBackground(index: number) {
-    if(index === 1) {
-      return 'blue';
-    } else if(index === 2) {
-      return 'green';
-    } else if(index === 3){
-      return 'red';
-    } else if(index === 4){
-      return 'yellow';
-    } else if(index === 5){
-      return 'purple';
-    } else {
-      return 'white';
-    }
-  }
-
 }
--- front/src/app/components/grid/grid.component.ts
+++ front/src/app/components/grid/grid.component.ts
@@ -5,25 +5,48 @@ import { NgFor } from '@angular/common';
 @Component({
   selector: 'grid',
   standalone: true,
-  imports: [
-    CellComponent,
-    NgFor
-
-  ],
+  imports: [CellComponent, NgFor],
   templateUrl: './grid.component.html',
-  styleUrl: './grid.component.css'
+  styleUrl: './grid.component.css',
 })
 export class GridComponent {
 
-  @Input() currentValue!: number;
-  cells: number[] = Array(100).fill(0);  // Initialise un tableau de 100 cellules avec une valeur de 0
 
-  changeCellValues(index: number) {
-    this.cells[index] = this.currentValue;
+  @Input() currentValue!: number;
+  cells: number[][] = []; //initialiser un tableau vide
+
+  constructor() {
+    this.generateGrid();
   }
+
+  generateGrid() {
+    for (let i = 0; i < 10; i++) { //generer les 10 colones du tableau
+      this.cells[i] = []; 
+      for (let j = 0; j < 10; j++) { //initialiser les cell des colone du tableau a 0
+        this.cells[i][j] = 0;
+      }
+    }
+  }
+
+  changeCellValues(rowIndex: number, colIndex: number) {
+    this.cells[rowIndex][colIndex] = this.currentValue; //changer la valeur de la cell
+    console.log(this.cells);
+  }
+
+  changeBackground(rowIndex: number, colIndex: number) {
+    switch (this.cells[rowIndex][colIndex]) { //changer la couleur de la cell
+      case 0: return 'white';
+      case 1: return 'blue';
+      case 2: return 'green';
+      case 3: return 'yellow';
+      case 4: return 'red';
+      case 5: return 'black';
+      default: return 'white';
+    }
+  }
+
   
   resetGrid() {
-    this.cells = Array(100).fill(0);
+    this.generateGrid();
   }
-  
 }
